88bd08f481fb1cac35a4fa74f993d80d69b539d6
seaofocean <xianyan.jia@intel.com>
2016-08-02 08:03:00 +0800

    update evaluation

--- ie/src/evaluation/NerEvaluation.scala
+++ ie/src/evaluation/NerEvaluation.scala
@@ -6,14 +6,15 @@ import scala.io.Source
   * Created by xianyan on 16-7-27.
   */
 
-class NerEvaluation {
-  var tp = 0
-  var fp = 0
-  var fn = 0
-  var tn = 0
-  var precision = 0.0
-  var recall = 0.0
-  var f1 = 0.0
+class NerEvaluation(var tp: Int, var fp: Int, var fn: Int, var tn: Int) {
+
+  def this() {
+    this(0, 0, 0, 0)
+  }
+
+  //  var precision = 0.0
+  //  var recall = 0.0
+  //  var f1 = 0.0
 
   /**
     * evaluates the precision, recall and f1
@@ -22,23 +23,35 @@ class NerEvaluation {
     * @param truths  List of true labels of one sentence
     * @param labels  list of labels to be checked
     */
-  def eval(outputs: List[String], truths: List[String], labels: List[String], original: String): Unit = {
-    println(original)
-    print("output: ");
-    outputs.foreach(printf("%15s", _));
-    println()
-    print("truths: ");
-    truths.foreach(printf("%15s", _));
-    println()
+  def eval(outputs: List[String], truths: List[String], labels: List[String], original: List[String]): Unit = {
+    //    println(original)
+    //    print("output: ");
+    //    outputs.foreach(printf("%15s", _));
+    //    println()
+    //    print("truths: ");
+    //    truths.foreach(printf("%15s", _));
+    //    println()
 
-    for ((o, t) <- (outputs zip truths)) {
+    for (((o, t), r) <- (outputs zip truths zip original)) {
       if (t == "o") {
         //truth negative
         if (o == "o") tn += 1 else fp += 1
       }
       else if (labels.contains(t)) {
         //truth positive
-        if (o == t) tp += 1 else fn += 1
+        if (o == t)
+          tp += 1
+        else {
+          fn += 1
+          //todo: not aligned for original sentence
+          if(!r.trim.isEmpty)
+          println(
+            Console.RED + r +
+              Console.BLACK + " not recognized as " +
+              Console.RED + t +
+              Console.BLACK + " in sentence " +
+              Console.BLUE + original.mkString(" "))
+        }
       }
     }
   }
@@ -55,7 +68,7 @@ class NerEvaluation {
     eval(outputStr.split(splitSign).map(_.trim).map(_.toLowerCase).toList,
       truthStr.split(splitSign).map(_.trim).map(_.toLowerCase).toList,
       labelStr.split(splitSign).map(_.trim).map(_.toLowerCase).toList,
-      original)
+      original.split(splitSign).map(_.trim).toList)
   }
 
   /**
@@ -71,19 +84,28 @@ class NerEvaluation {
     for (((oLine, tLine), original) <- (outputs zip truths zip originalLines)) {
       eval(oLine, tLine, labelStr, delimiter, original)
     }
-    precision = tp * 1.0 / (tp + fp)
-    recall = tp * 1.0 / (tp + fn)
-    f1 = 2 * (recall * precision) / (recall + precision)
+    //    precision = tp * 1.0 / (tp + fp)
+    //    recall = tp * 1.0 / (tp + fn)
+    //    f1 = 2 * (recall * precision) / (recall + precision)
   }
 
+  def precision = tp * 1.0 / (tp + fp)
+
+  def recall = tp * 1.0 / (tp + fn)
+
+  def f1 = 2 * (recall * precision) / (recall + precision)
+
+  //  def precision(tp: Int, fp: Int) =  tp * 1.0 / (tp + fp)
+  //  def recall(tp: Int, fn: Int) = tp * 1.0 / (tp + fn)
+  //  def f1(precision: Int, recall: Int) = 2 * (recall * precision) / (recall + precision)
   /**
     * read the labeled truths from the file
     *
     * @param file file path
     * @return a list of labels correspond to each line
     */
-  def transformFromFile(file: String): List[String] = {
-    transform(Source.fromFile(file).getLines.toList)
+  def transformLabelFromFile(file: String): List[String] = {
+    transformLabel(Source.fromFile(file).getLines.toList)
   }
 
   /**
@@ -92,8 +114,8 @@ class NerEvaluation {
     * @param labeled a list of sentence labeled
     * @return a list of labels
     */
-  private def transform(labeled: List[String]): List[String] =
-    for(line <- labeled) yield transform(line)
+  private def transformLabel(labeled: List[String]): List[String] =
+  for (line <- labeled) yield transformLabel(line)
 
   /**
     * extract labels from the sentence
@@ -101,9 +123,35 @@ class NerEvaluation {
     * @param line one line with label for each word
     * @return label string separated by ","
     */
-  private def transform(line: String): String = {
+  private def transformLabel(line: String): String = {
     val items = line.split("\t")
-    val tranStr = for(item <- items) yield item.split("/")(1)
+    val tranStr = for (item <- items; eles = item.split("/") if (eles.length > 1)) yield eles(1)
+    tranStr.mkString(", ")
+  }
+
+  def transformTextFromFile(file: String): List[String] = {
+    transformText(Source.fromFile(file).getLines.toList)
+  }
+
+  /**
+    * extract the label from the string list
+    *
+    * @param labeled a list of sentence labeled
+    * @return a list of labels
+    */
+  private def transformText(labeled: List[String]): List[String] = {
+    for (line <- labeled) yield transformText(line)
+  }
+
+  /**
+    * extract labels from the sentence
+    *
+    * @param line one line with label for each word
+    * @return label string separated by ","
+    */
+  private def transformText(line: String): String = {
+    val items = line.split("\t")
+    val tranStr = for (item <- items; eles = item.split("/") if (eles.length > 1)) yield eles(0).trim
     tranStr.mkString(", ")
   }
 }
