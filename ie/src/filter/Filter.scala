bf6f4e33321b5bbfed039c17cb9dda75acb2765e
seaofocean <xianyan.jia@intel.com>
2016-08-03 19:41:11 +0800

    add filter code

---
+++ ie/src/filter/Filter.scala
@@ -0,0 +1,22 @@
+package filter
+
+import scala.io.Source
+
+/**
+  * Created by xianyan on 8/3/16.
+  */
+object Filter {
+  private val badWords = Source.fromFile("data/filter/badWords.txt").getLines().toSet
+
+  def containBadWord(string: String): Boolean = {
+    val words = string.split("\\s+|,|;|\\.")
+    for( word <- words ) {
+      if (badWords.contains(word)) return true
+    }
+    false
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(containBadWord("he is a. bitch."))
+  }
+}
