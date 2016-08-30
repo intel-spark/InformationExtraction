17bf041e90be3374de9a838122599e4ba8e9ed97
seaofocean <xianyan.jia@intel.com>
2016-08-30 20:00:31 +0800

    add rules for title

--- ie/src/main/java/com/intel/ie/analytics/IntelKBPConfig.java
+++ ie/src/main/java/com/intel/ie/analytics/IntelKBPConfig.java
@@ -3,17 +3,19 @@ package com.intel.ie.analytics;
 public class IntelKBPConfig {
 
     public static String Regex_NER_cased = "data/kbp/regexner_cased.tab";
-    public static String Regex_NER_caseless = "data/kbp/regexner_caseless.tab";    
+    public static String Regex_NER_caseless = "data/kbp/regexner_caseless.tab";
+    public static String Regex_NER_more = "data/kbp/regexner_more.tab";
 
     public static String combined = Regex_NER_caseless
-            + ";" + Regex_NER_cased;
+            + ";" + Regex_NER_cased
+            + ";" + Regex_NER_more;
 
     public static String KBP_TOKENSREGEX_DIR = "data/kbp/tokensregex";
     public static String KBP_SEMGREX_DIR = "data/kbp/semgrex";
 
     public static String KBP_CLASSIFIER = "data/kbp/tac-re-lr.ser.gz";
 
-    public static IntelEnsembleStrategy ENSEMBLE_STRATEGY = IntelEnsembleStrategy.HIGH_PRECISION;
-    
+    public static IntelEnsembleStrategy ENSEMBLE_STRATEGY = IntelEnsembleStrategy.HIGH_RECALL;
+
     public static boolean bSeprateFormerTitle = false;
 }
