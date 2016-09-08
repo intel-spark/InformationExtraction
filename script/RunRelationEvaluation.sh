d7afdd10696316a012087a7a473a4b0fb21eb16c
Yuhao <yuhao.yang@intel.com>
2016-09-08 12:30:48 -0700

    update script

--- script/RunRelationEvaluation.sh
+++ script/RunRelationEvaluation.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TargetJar=target/ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
+TargetJar=ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
 MainClass=org.apache.spark.sql.RelationEvaluation
-dataPath=data/evaluation/RelationEvaluation/web
-labelPath=data/evaluation/RelationEvaluation/extraction
+dataPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/web/
+labelPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/extraction/
 
 spark-submit \
   --master yarn \
@@ -11,11 +11,12 @@ spark-submit \
   --executor-memory 20g \
   --num-executors 4 \
   --class $MainClass \
-  --jars lib/stanford-english-corenlp-models-current.jar,intel-resources.jar \
+  --jars stanford-english-corenlp-models-current.jar \
   --files config.properties \
   $TargetJar \
   --partitionSize 8 \
   --withDetail false \
   $dataPath \
   $labelPath
+  
   
--- script/RunSparkBatchDriver.sh
+++ script/RunSparkBatchDriver.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 SPARK_HOME=/opt/spark-1.6.2-bin-hadoop2.6
-TargetJar=target/ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
+TargetJar=ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
 MainClass=com.intel.ie.SparkBatchDriver
-
 
 $SPARK_HOME/bin/spark-submit \
   --master yarn \
@@ -11,6 +10,7 @@ $SPARK_HOME/bin/spark-submit \
   --executor-memory 20g \
   --num-executors 4 \
   --class $MainClass \
-  --jars lib/stanford-english-corenlp-models-current.jar,intel-resources.jar \
+  --jars stanford-english-corenlp-models-current.jar \
   --files config.properties \
   $TargetJar 8
+  
