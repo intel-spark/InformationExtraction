5a59a9cc3ae772ef1c20d265b3ab66d8acc36d2a
Yuhao <yuhao.yang@intel.com>
2016-09-12 14:30:33 -0700

    config update

--- script/RunRelationEvaluation.sh
+++ script/RunRelationEvaluation.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+SPARK_HOME=/opt/spark-1.6.2-bin-hadoop2.6
 TargetJar=ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
 MainClass=org.apache.spark.sql.RelationEvaluation
-dataPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/web/
+dataPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/web/AECOM
 labelPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/extraction/
 
-spark-submit \
+$SPARK_HOME/bin/spark-submit \
   --master yarn \
   --driver-memory 20g \
   --executor-memory 20g \
