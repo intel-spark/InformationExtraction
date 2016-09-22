8c0cf20121f5bf161fed9bc0c5fa05d05cae47da
Yuhao Yang <hhbyyh@gmail.com>
2016-09-22 10:53:43 -0700

    prepare for maven repository

--- script/RunRelationEvaluation.sh
+++ script/RunRelationEvaluation.sh
@@ -3,7 +3,7 @@
 # Change the SPARK_HOME and dataPath, labelPath according to your setting.
 
 SPARK_HOME=/opt/spark-1.6.2-bin-hadoop2.6
-TargetJar=ie-project-1.0-SNAPSHOT-jar-with-dependencies.jar
+TargetJar=InformationExtraction-1.0-SNAPSHOT-jar-with-dependencies.jar
 MainClass=org.apache.spark.sql.RelationEvaluation
 dataPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/web/
 labelPath=hdfs://172.168.2.181:9000/user/yuhao/JPMC/data/evaluation/extraction/
